16586fc19732b3b24958ba22086ba925b82479f2
Nolan Knight <nolanknight2026@u.northwestern.edu>
2026-02-06 21:10:18 -0600

    Finished Task E.1. Subscribers and publishers are now created.

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -1,160 +1,116 @@
 /// \file
 /// \brief Creates a ROS 2 Node which enables control of the turtlebot via geometry/msg/Twist messages on the cmd_vel topic.
-///     robot.x0 (double, default: 0.0): Initial x position of the robot in the simulation
-///     robot.y0 (double, default: 0.0): Initial y position of the robot in the simulation
-///     robot.theta0 (double, default: 0.0): Initial orientation of the robot in the simulation (in radians)
-///     arena_x_length (double, default: 8.0): Length of the arena in the x direction (in meters)
+///
+/// PARAMETERS:
+///     wheel_radius (double): The radius of the robot's wheels in meters
+///     track_width (double): The distance between the robot's wheels in meters
+///     motor_cmd_max (int): The maximum motor command that can be sent to the robot
+///     motor_cmd_per_rad_sec (double): The conversion factor from wheel angular velocity in radians per second to motor command units
+///     encode_ticks_per_rad (int): The number of encoder ticks per radian of wheel rotation     
 /// PUBLISHES:
-///     ~
+///     ~ wheel_cmd (nuturtle_control/msg/WheelCommands): The motor commands for the left and right wheels, converted from the cmd_vel topic
+///     ~ joint_states (sensor_msgs/msg/JointState): The current joint states of the
 /// SUBSCRIBES:
-///     None
+///     ~ cmd_vel (geometry_msgs/msg/Twist): The desired linear and angular velocity of the robot base, which is converted to wheel commands and published on the wheel_cmd topic
+///     ~ sensor_data (nuturtle_control/msg/SensorData): The current wheel encoder ticks, which is used to update the DiffDrive model and publish the current joint states on the joint_states
 /// SERVERS:
-///     ~
+///     None
 /// CLIENTS:
 ///     None
 #include <memory>
 #include <string>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/u_int64.hpp"
-#include "std_srvs/srv/empty.hpp"
-#include "tf2_ros/transform_broadcaster.hpp"
-#include <tf2/LinearMath/Quaternion.hpp>
-#include "geometry_msgs/msg/transform_stamped.hpp"
-#include <visualization_msgs/msg/marker.hpp>
-#include <visualization_msgs/msg/marker_array.hpp>
-#include <vector>
-
-using namespace std::chrono_literals;
+#include "geometry_msgs/msg/twist.hpp"
+#include "nuturtle_control/msg/wheel_commands.hpp"
+#include "nuturtle_control/msg/sensor_data.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "turtlelib/diff_drive.hpp"
 
 /// \brief A class to launch a Simulator Node
-class Nusimulator : public rclcpp::Node {
+class TurtleControl : public rclcpp::Node {
 public:
-  Nusimulator()
-  : Node("nusimulator")
-  {
-    // Initialize the Parameter:
-    auto rate = this->declare_parameter<double>("rate", 100.0);
-    // Initialize the ~/timestep Publisher:
-    publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
-    // Initialize the ~/real_walls and ~/real_obstacles Publishers:
-    marker_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("~/real_walls",
-      rclcpp::QoS(10).transient_local());
-    obst_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("~/real_obstacles",
-      rclcpp::QoS(10).transient_local());
-    // Initialize the transform broadcaster
-    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    TurtleControl()
+    : Node("turtle_control") {
+        // Initialize the Parameters:
+        wheel_radius_ = declare_parameter<double>("wheel_radius", 0.033);
+        track_width_ = declare_parameter<double>("track_width", 0.033);
+        motor_cmd_max_ = declare_parameter<int>("motor_cmd_max", 265);
+        motor_cmd_per_rad_sec_ = declare_parameter<double>("motor_cmd_per_rad_sec", 41.67);
+        encode_ticks_per_rad_ = declare_parameter<int>("encode_ticks_per_rad", 652);
+        
+        // Check to make sure  that all parameters are valid:
+        if (wheel_radius_ <= 0 || track_width_ <= 0 || motor_cmd_max_ <= 0 || motor_cmd_per_rad_sec_ <= 0 || encode_ticks_per_rad_ <= 0) {
+            // Log an error message and throw an exception if any parameter is invalid
+            RCLCPP_ERROR_STREAM_ONCE(this->get_logger(), "Not all parameters were initialized:" <<  
+            " wheel_radius=" << wheel_radius_ << ", track_width=" << track_width_ << ", motor_cmd_max=" << motor_cmd_max_
+            << ", motor_cmd_per_rad_sec=" << motor_cmd_per_rad_sec_ << ", encode_ticks_per_rad=" << encode_ticks_per_rad_);
+            throw std::runtime_error("Parameters not initialized"); // Stops the node from launching if parameters are not valid.
+        }
 
-    // Initialize and publish the Real Walls:
-    auto marker_array_walls = createWalls();
-    marker_pub_->publish(marker_array_walls);
-    // Initialize and publish the Real Obstacles:
-    auto marker_array_obstacles = createObstacles();
-    obst_pub_->publish(marker_array_obstacles);
+        // Contruct the DiffDrive model using the wheel radius and track width parameters:
+        diff_drive_ = turtlelib::DiffDrive(wheel_radius_, track_width_);
 
-    // Create the timer callback:
-    auto timer_callback = [this, rate]() -> void {
-        // Print a Message Once:
-        RCLCPP_INFO_ONCE(this->get_logger(), "The Timer rate is %f!", rate);
-        // Publisher:
-        auto message = std_msgs::msg::UInt64();
-        message.data = timestep_++;
-        RCLCPP_DEBUG(this->get_logger(), "Timestep: '%lu'", message.data);
-        this->publisher_->publish(message);
+        // Construct the publisher for wheel commands:
+        wheel_cmd_publisher_ = this->create_publisher<nuturtle_control::msg::WheelCommands>("wheel_cmd", 10);
+        // Construct the publisher for joint states:
+        joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+        
+        // Construct the subscriber and publisher for cmd_vel:
+        cmd_vel_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist> ("cmd_vel", 10, [this](const geometry_msgs::msg::Twist::SharedPtr msg) {
+            // Convert the Twist message to wheel commands:
+            turtlelib::Twist2D twist{turtlelib::Vector2D{msg->linear.x, msg->linear.y}, msg->angular.z};
+            turtlelib::wheel_vel wheel_vel = this->diff_drive_.compute_ik(twist);
 
-        // Assign parameters to corresponding tf variables and broadcast:
-        geometry_msgs::msg::TransformStamped t;
-        t.header.stamp = this->get_clock()->now();
-        t.header.frame_id = "nusim/world";
-        t.child_frame_id = "red/base_footprint";
-        // Turtlebot only exists in 2D, thus we get x and y translation
-        // coordinates from the message and set the z coordinate to 0
-        t.transform.translation.x = x0_;
-        t.transform.translation.y = y0_;
-        t.transform.translation.z = 0.0;
+            // Convert the wheel speeds to motor commands and clamp them to the maximum allowed value:
+            auto left_motor_cmd = std::clamp(static_cast<int>(wheel_vel.v_lw * this->motor_cmd_per_rad_sec_), -this->motor_cmd_max_, this->motor_cmd_max_);
+            auto right_motor_cmd = std::clamp(static_cast<int>(wheel_vel.v_rw * this->motor_cmd_per_rad_sec_), -this->motor_cmd_max_, this->motor_cmd_max_);
 
-        // For the same reason, turtlebot can only rotate around one axis
-        // and this why we set rotation in x and y to 0 and obtain
-        // rotation in z axis from the message
-        tf2::Quaternion q;
-        q.setRPY(0, 0, theta0_);
-        t.transform.rotation.x = q.x();
-        t.transform.rotation.y = q.y();
-        t.transform.rotation.z = q.z();
-        t.transform.rotation.w = q.w();
-        // Send the transformation
-        tf_broadcaster_->sendTransform(t);
-      };
-    // Set the timer:
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(static_cast<int>(1000 / rate)),
-      timer_callback);
-    // Reset service
-    reset_service_ = this->create_service<std_srvs::srv::Empty>(
-            "~/reset",
-            std::bind(&Nusimulator::handle_service_reset, this, std::placeholders::_1,
-      std::placeholders::_2)
-    );
-  }
+            // Publish the wheel commands to the wheel_cmd topic:
+            nuturtle_control::msg::WheelCommands wheel_cmd_msg;
+            wheel_cmd_msg.left_velocity = left_motor_cmd;
+            wheel_cmd_msg.right_velocity = right_motor_cmd;
+            wheel_cmd_publisher_->publish(wheel_cmd_msg);
+        });
+        
+        // Construct the subscriber for sensor data to joint state publisher:
+        sensor_data_subscriber_ = this->create_subscription<nuturtle_control::msg::SensorData>("sensor_data", 10, [this](const nuturtle_control::msg::SensorData::SharedPtr msg) {
+            // Update the DiffDrive model with the new wheel encoder data using forward kinematics:
+            auto phi_left = static_cast<double>(msg->left_encoder) / this->encode_ticks_per_rad_;
+            auto phi_right = static_cast<double>(msg->right_encoder) / this->encode_ticks_per_rad_;
+            this->diff_drive_.update_fk(phi_left, phi_right);
+            // Publish the joint state for the left and right wheel joints:
+            sensor_msgs::msg::JointState joint_state_msg;
+            joint_state_msg.header.stamp = msg->stamp;
+            joint_state_msg.name.push_back("wheel_left_joint");
+            joint_state_msg.position.push_back(phi_left);
+            joint_state_msg.name.push_back("wheel_right_joint");
+            joint_state_msg.position.push_back(phi_right);
+            joint_state_publisher_->publish(joint_state_msg);
+    });
+    }
 
 private:
-  /// \brief Resets the simulation timestep and robot initial pose
-  ///
-  /// Resets timestep_ to 0 and sets x0_, y0_, theta0_ to their declared parameters.
-  ///
-  /// \param request - Empty request from the std_srvs::srv::Empty service
-  /// \param response - Empty response from the std_srvs::srv::Empty service
-  /// \returns void
-  void handle_service_reset(
-    const std::shared_ptr<std_srvs::srv::Empty::Request> request,
-    const std::shared_ptr<std_srvs::srv::Empty::Response> response)
-  {
-    (void)request;
-    (void)response;
-    RCLCPP_INFO(this->get_logger(), "Timestep Reset!");
-    timestep_ = 0;                  // Reset the timestep counter
-    // ####################### Begin_Citation [7] ############################
-    // Reset the simulation positions:
-    x0_ = this->get_parameter("x0").as_double();
-    y0_ = this->get_parameter("y0").as_double();
-    theta0_ = this->get_parameter("theta0").as_double();
-    // ######################## End_Citation [7] ###############################
-  }
+    // Initialize ROS 2 Infrustructure:
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscriber_;
+    rclcpp::Publisher<nuturtle_control::msg::WheelCommands>::SharedPtr wheel_cmd_publisher_;
+    rclcpp::Subscription<nuturtle_control::msg::SensorData>::SharedPtr sensor_data_subscriber_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher_;
 
-  // Initialize ROS 2 Infrustructure:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
-  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
-  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr obst_pub_;
-  rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_service_;
-  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    // Initialize the parameters:
+    double wheel_radius_;
+    double track_width_;
+    int motor_cmd_max_;
+    double motor_cmd_per_rad_sec_;
+    int encode_ticks_per_rad_;
 
-  // Set the timestep:
-  uint64_t timestep_;
-
-  // Set the Ground Truth values:
-  double x0_ = this->declare_parameter<double>("robot.x0", 0.0);
-  double y0_ = this->declare_parameter<double>("robot.y0", 0.0);
-  double theta0_ = this->declare_parameter<double>("robot.theta0", 0.0);
-
-  // Set the Arena Wall Dimensions:
-  double arena_x_ = this->declare_parameter<double>("arena_x_length", 8.0);
-  double arena_y_ = this->declare_parameter<double>("arena_y_length", 12.0);
-  double arena_thick_ = this->declare_parameter<double>("arena_thickness", 0.25);
-
-  // Set the Obstacle Dimensions:
-  std::vector<double> obstacles_x_ = this->declare_parameter<std::vector<double>>("obstacles_x",
-    std::vector<double>{});
-  std::vector<double> obstacles_y_ = this->declare_parameter<std::vector<double>>("obstacles_y",
-    std::vector<double>{});
-  std::vector<double> obstacles_r_ = this->declare_parameter<std::vector<double>>("obstacles_r",
-    std::vector<double>{});
-  double obst_height_ = this->declare_parameter<double>("obstacle_height", 0.25);
+    // Initialize the DiffDrive model:
+    turtlelib::DiffDrive diff_drive_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Nusimulator>());
+  rclcpp::spin(std::make_shared<TurtleControl>());
   rclcpp::shutdown();
   return 0;
 }
