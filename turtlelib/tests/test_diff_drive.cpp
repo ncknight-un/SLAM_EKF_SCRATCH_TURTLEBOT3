4e732ece9dd251ab480e603bdc823280ab7fa5ca
Nolan Knight <nolanknight2026@u.northwestern.edu>
2026-02-11 11:46:17 -0600

    Fixed test_diff_drive from the updates I made over the weekend. Realized I had written the test wrong. Also had Matt helped me fix my test links. I just need to write my tests now!

--- turtlelib/tests/test_diff_drive.cpp
+++ turtlelib/tests/test_diff_drive.cpp
@@ -11,14 +11,14 @@ constexpr double EPS = 0.001;
 // Forward Path:
 TEST_CASE("Forward Path", "[update_fk and compute_ik]") {
     // Create a DiffDrive model with a wheel track of 0.1m and wheel radius of 0.01m.   
-    turtlelib::DiffDrive diff_drive(0.1, 0.01);
+    turtlelib::DiffDrive diff_drive(1.0, 1.0);
 
     SECTION("Forward Kinematics for simple forward path") {
         // Update the wheel positions to move the robot forward 0.2 meters in a straight path.
-        diff_drive.update_fk(20.0, 20.0);
+        diff_drive.update_fk(1.0, 1.0);
         // Check that the robot base transform is updated correctly:
         turtlelib::Transform2D q = diff_drive.get_q();
-        REQUIRE_THAT(q.translation().x, WithinRel(0.2, EPS));   // The robot should have moved forward 0.2 meters in the x direction in its frame 
+        REQUIRE_THAT(q.translation().x, WithinRel(1.0, EPS));   // The robot should have moved forward 0.2 meters in the x direction in its frame 
         REQUIRE_THAT(q.translation().y, WithinRel(0.0, EPS));
         REQUIRE_THAT(q.rotation(), WithinRel(0.0, EPS));
     }
@@ -29,8 +29,8 @@ TEST_CASE("Forward Path", "[update_fk and compute_ik]") {
         turtlelib::wheel_vel vel = diff_drive.compute_ik(twist);
 
         // Check that the required wheel angular velocities are correct:
-        REQUIRE_THAT(vel.v_lw, WithinRel(20, EPS));   // Right wheel should rotate at 20 rad/s
-        REQUIRE_THAT(vel.v_rw, WithinRel(20, EPS));   // Left wheel should rotate at 20 rad/s
+        REQUIRE_THAT(vel.v_lw, WithinRel(0.2, EPS));   // Right wheel should rotate at 20 rad/s
+        REQUIRE_THAT(vel.v_rw, WithinRel(0.2, EPS));   // Left wheel should rotate at 20 rad/s
     }
 };
 
@@ -41,12 +41,12 @@ TEST_CASE("Pure Rotation Path", "[update_fk and compute_ik]") {
 
     SECTION("Forward Kinematics for simple rotation path") {
         // Update the wheel positions to only rotate the robot -2 radians in place.
-        diff_drive.update_fk(10.0, -10.0);
+        diff_drive.update_fk(1.0, -1.0);
         // Check that the robot base transform is updated correctly:
         turtlelib::Transform2D q = diff_drive.get_q();
         REQUIRE_THAT(q.translation().x, WithinRel(0.0, EPS));   // The robot should have moved forward 0.0 meters in the x direction in its frame 
         REQUIRE_THAT(q.translation().y, WithinRel(0.0, EPS));
-        REQUIRE_THAT(q.rotation(), WithinRel(-2.0, EPS));  // The robot should have rotated -2 radians in place
+        REQUIRE_THAT(q.rotation(), WithinRel(-0.2, EPS));  // The robot should have rotated -1 radians in place
     }
 
     SECTION("Inverse Kinematics for simple rotation path") {
@@ -55,24 +55,25 @@ TEST_CASE("Pure Rotation Path", "[update_fk and compute_ik]") {
         turtlelib::wheel_vel vel = diff_drive.compute_ik(twist);
 
         // Check that the required wheel angular velocities are correct:
-        REQUIRE_THAT(vel.v_lw, WithinRel(-10, EPS));   // Right wheel should rotate at -10 rad/s
-        REQUIRE_THAT(vel.v_rw, WithinRel(10, EPS));  // Left wheel should rotate at 10 rad/s
+        REQUIRE_THAT(vel.v_lw, WithinRel(10, EPS));   // Right wheel should rotate at -10 rad/s
+        REQUIRE_THAT(vel.v_rw, WithinRel(-10, EPS));  // Left wheel should rotate at 10 rad/s
     }
 };
 
 // Arc Path:
 TEST_CASE("Arc Path", "[update_fk and compute_ik]") {
     // Create a DiffDrive model with a wheel track of 0.1m and wheel radius of 0.01m.   
-    turtlelib::DiffDrive diff_drive(0.1, 0.01);
+    turtlelib::DiffDrive diff_drive(1.0, 1.0);
 
     SECTION("Forward Kinematics for simple arc path") {
         // Update the wheel positions to follow an arc path that moves the robot forward 1 meter and rotates it 2.0 rad to the right:
-        diff_drive.update_fk(110.0, 90.0);
+        diff_drive.update_fk(1.0, 2.0);
         // Check that the robot base transform is updated correctly:
         turtlelib::Transform2D q = diff_drive.get_q();
-        REQUIRE_THAT(q.translation().x, WithinRel(0.4546, EPS));   // The robot should have moved forward 1.0 meters in the x direction in its frame 
-        REQUIRE_THAT(q.translation().y, WithinRel(0.0, EPS));
-        REQUIRE_THAT(q.rotation(), WithinRel(2.0, EPS));  // The robot should have rotated -2 radians and moved forward along the path 1 meters
+        REQUIRE_THAT(q.translation().x, WithinRel(1.2622, EPS));
+        REQUIRE_THAT(q.translation().y, WithinRel(-0.6896, EPS));
+        REQUIRE_THAT(q.rotation(), WithinRel(1.0, EPS));
+// The robot should have rotated -2 radians and moved forward along the path 1 meters
     }
 
     SECTION("Inverse Kinematics for simple arc path") { 
@@ -81,8 +82,8 @@ TEST_CASE("Arc Path", "[update_fk and compute_ik]") {
         turtlelib::wheel_vel vel = diff_drive.compute_ik(twist);
 
         // Check that the required wheel angular velocities are correct:
-        REQUIRE_THAT(vel.v_lw, WithinRel(90.0, EPS));   // Right wheel should rotate at 90.0 rad/s
-        REQUIRE_THAT(vel.v_rw, WithinRel(110.0, EPS));  // Left wheel should rotate at 110.0 rad/s
+        REQUIRE_THAT(vel.v_lw, WithinRel(0.0, EPS));   // Right wheel should rotate at 90.0 rad/s
+        REQUIRE_THAT(vel.v_rw, WithinRel(2.0, EPS));  // Left wheel should rotate at 110.0 rad/s
     }
 };
 
